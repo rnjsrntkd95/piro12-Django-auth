ad523708fb06e8d4891893bc4e355d093b7f1387
KwonKusang <rnjsrntkd95@gmail.com>
2020-01-22 02:25:13 +0900

    07 회원가입과 동시에 로그인하기

--- accounts/views.py
+++ accounts/views.py
@@ -1,29 +1,50 @@
 from django.conf import settings
 from django.contrib.auth.models import User
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, resolve_url
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from .forms import SignupForm
+from django.contrib.auth import login as auth_login
 
+# FBV 함수 기반 뷰
 '''
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
             user = form.save()
-            return redirect(settings.LOGIN_URL)
+            # 로그인처리
+            auth_login(request, user)
+            # return redirect('profile')
+
+            
+            # return redirect(settings.LOGIN_URL)
     else:
         form = SignupForm()
     return render(request, 'accounts/signup.html', {
         'form': form,
     })
 '''
+
+
+class SignupView(CreateView):
+    model = User
+    form_class = SignupForm
+    template_name = 'accounts/signup.html'
+
+    def get_success_url(self):
+        return resolve_url('profile')
+
+    def form_valid(self, form):
+        user = form.save()
+        auth_login(self.request, user)
+        return redirect(self.get_success_url())
+
+
 # CBV 클라스 기반 뷰
-signup = CreateView.as_view(model=User,
-                            form_class=SignupForm,
-                            success_url='settings.LOGIN_URL',
-                            template_name='accounts/signup.html')
+signup = SignupView.as_view()
+
 
 @login_required
 def profile(request):
